0afad305ac85a793557723aa55652d60eb108319
louohr <louise.ohrner@gmail.com>
2024-09-06 14:25:07 +0200

    update themecontext

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,57 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+  Link,
+  Routes,
+} from "react-router-dom";
+import { ThemeProvider, useTheme } from "./context/ThemeContext";
+import Home from "./Home";
+import About from "./About";
+import "./app.css";
 
-// ThemeToggleButton component
+// Set up toggle theme button
 const ThemeToggleButton = () => {
   const { theme, toggleTheme } = useTheme();
 
+  // Toggle to change theme
   return <button onClick={toggleTheme}>Switch to {theme === "😎" ? "😴" : "😎"}</button>;
 };
 
-const App = () => {
-  return (
+// Set up the layout component
+const Layout = () => (
+  <div>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <ThemeToggleButton />
+    </nav>
 
-  );
-};
+    {/* Place where nested routes will render */}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </div>
+);
+
+// Set up data and paths
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<Layout />}>
+      {/* Define routes in the router configuration */}
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Route>
+  )
+);
+
+// Wrap the router with ThemeProvider
+const App = () => (
+  <ThemeProvider>
+    <RouterProvider router={router} />
+  </ThemeProvider>
+);
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 import "./index.css";
-import { ThemeProvider } from "./ThemeContext.tsx";
-import { Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <Router router={router} />
-    </ThemeProvider>
+    <App />
   </StrictMode>
 );
